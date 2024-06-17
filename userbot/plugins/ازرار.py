095db2417a110b01d14ed2ba613b021cf0b8d3d3
letonn <164564472+letonn@users.noreply.github.com>
2024-06-17 19:08:11 +0300

    Update ازرار.py

--- userbot/plugins/ازرار.py
+++ userbot/plugins/ازرار.py
@@ -108,10 +108,10 @@ CMD_HELP.update(
     \n\n**Button post helper**\
     \n•  **Syntax : **`.كول`\
     \n•  **Function :** __For working of this you need your bot({BOT_USERNAME}) in the group/channel you are using and Buttons must be in the format as [Name on button]<buttonurl:link you want to open> and markdown is Default to html__\
-    \n•  **Example :** `.كول + الجمله المراد اظهارها [المطـور]<buttonurl:https://t.me/ZlZZl77> [زد ثـون]<buttonurl:https://t.me/ZedThon> [اوامر السورس]<buttonurl:https://t.me/Zed_Thon>`\
+    \n•  **Example :** `.كول + الجمله المراد اظهارها [المطـور]<buttonurl:https://t.me/S_1_02> [ليثون]<buttonurl:https://t.me/A1DIIU> [اوامر السورس]<buttonurl:https://t.me/A1DIIU>`\
     \n\n•  **Syntax : **`.كوول`\
     \n•  **Function :** __Buttons must be in the format as [Name on button]<buttonurl:link you want to open>__\
-    \n•  **Example :** `.كوول + الجمله المراد اظهارها [المطـور]<buttonurl:https://t.me/ZlZZl77> [زد ثـون]<buttonurl:https://t.me/ZedThon> [اوامر السورس]<buttonurl:https://t.me/Zed_Thon>`\
+    \n•  **Example :** `.كوول + الجمله المراد اظهارها [المطـور]<buttonurl:https://t.me/S_1_02> [ ليثون]<buttonurl:https://t.me/A1DIIU> [اوامر السورس]<buttonurl:https://t.me/A1DIIU>`\
     "
     }
 )
